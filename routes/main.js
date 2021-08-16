8edf8b9c7d46b90c0757b1c5d163811cbe5fcd1c
saadmanfarhad <saadmanheavy@gmail.com>
2021-08-16 20:34:34 +0200

    Completed all task requirements

--- routes/main.js
+++ routes/main.js
@@ -3,9 +3,17 @@ const User = mongoose.model("users");
 const Exercise = mongoose.model("exercises");
 
 module.exports = (app) => {
+  app.get("/api/users", async (req, res) => {
+    try {
+      const users = await User.find({});
+      res.send(users);
+    } catch (e) {
+      res.status(422).send({ status: false, message: e });
+    }
+  });
+
   app.post("/api/users", async (req, res) => {
     const { username } = req.body;
-    console.log(username);
 
     const newUser = new User({
       username,
@@ -13,7 +21,6 @@ module.exports = (app) => {
 
     try {
       const user = await newUser.save();
-      console.log(user);
       res.send({ username: user.username, _id: user._id });
     } catch (e) {
       if (e.code === 11000) {
@@ -22,5 +29,64 @@ module.exports = (app) => {
           .send({ status: false, message: "Username already exists" });
       }
     }
+  });
+
+  app.post("/api/users/:_id/exercises", async (req, res) => {
+    const { id, description, duration, date } = req.body;
+
+    if (id && description && duration) {
+      const newExercise = new Exercise({
+        user: id,
+        description,
+        duration,
+        date: date ? date : new Date(),
+      });
+
+      try {
+        const exercise = await newExercise.save();
+        const user = await User.findOne({ _id: exercise.user });
+
+        res.send({
+          _id: exercise.id,
+          username: user.username,
+          duration: exercise.duration,
+          description: exercise.description,
+          date: exercise.date.toDateString(),
+        });
+      } catch (e) {
+        res.status(422).send({ status: false, message: e });
+      }
+    }
+  });
+
+  app.get("/api/users/:_id/logs", async (req, res) => {
+    const { _id } = req.params;
+    const { limit, to, from } = req.query;
+    const user = await User.findOne({ _id: _id });
+
+    const query =
+      to && from
+        ? {
+            user: _id,
+            date: { $gte: from, $lte: to },
+          }
+        : { user: _id };
+
+    const exercises = await Exercise.find(query).limit(parseInt(limit));
+
+    const logs = exercises.map((exercise) => {
+      return {
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString(),
+      };
+    });
+
+    res.send({
+      _id: user._id,
+      username: user.username,
+      count: exercises.length,
+      logs: logs,
+    });
   });
 };
